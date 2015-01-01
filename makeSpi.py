4213960ffef304c929304b7e96594b846d02cfa2
Matt Venn <matt@mattvenn.net>
2015-01-01 14:21:32 +0000

    a bit more user friendly

--- makeSpi.py
+++ makeSpi.py
@@ -4,10 +4,10 @@ import sys
 
 spi_file = "spi.json"
 data = {}
-fields = [ "sort", "account", "codes" , "mem_day" , "mem_month" , "mem_year" , "first_school" , "last_school" , "memorable_name" , "place_of_birth" ]
+fields = [ ("sort","6 digit sort code (no spaces or hyphens)"), ("account","8 digit account number"), ("codes","your online pin number") , ("mem_day","2 digit day of month of your memorable date") , ("mem_month","2 digit month of year of your memorable date") , ("mem_year","4 digit year of your memorable date") , ("first_school","first school") , ("last_school","last school") , ("memorable_name","memorable name") , ("place_of_birth","place of birth") ]
 for field in fields:
-    print "%s:" % field
-    data[field] = sys.stdin.readline().strip()
+    print "%s:" % field[1]
+    data[field[0]] = sys.stdin.readline().strip()
 
 f = open(spi_file, 'w')
 json.dump(data,f)
